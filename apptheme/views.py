9f819114c60326d7d59703d1be1ae45ece444d81
lakshay7777 <lakshaykhatri0101@gmail.com>
2024-09-06 20:08:47 +0530

    theme project update

---
+++ apptheme/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+
+
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+def blog(request):
+    return render(request, 'blog.html')

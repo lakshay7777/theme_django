62194ff286b2c464cb290e32b5b325ef584aaf6a
lakshay7777 <lakshaykhatri0101@gmail.com>
2024-09-23 15:00:21 +0530

    useing captcha

--- apptheme/urls.py
+++ apptheme/urls.py
@@ -1,19 +1,14 @@
-from django.urls import path 
-from .import views
+from django.urls import path
+from . import views
 
 urlpatterns = [
     path('category/<str:slug>/', views.category_blog_list, name='category_blog_list'),
-    
     path('tag/<str:slug>/', views.tag_blog_list, name='tag_blog_list'),
-### path('homedetails/',views.home_detail, name='home-details'),
     path('blogdetails/<str:slug>/', views.blog_detail, name='blog_details'),
-
-    path('ContactUs/',views.contact, name='contact-us'),
-    path('AboutUs/',views.about, name='about-us'),
-    path('blog/',views.blog, name='blog'),
-    path('',views.home, name='home'),
-
+    path('ContactUs/', views.contact, name='contact-us'),
+    path('AboutUs/', views.about, name='about-us'),
+    path('blog/', views.blog, name='blog'),
+    path('', views.home, name='home'),
     path('contact/', views.contact, name='contact'),
-
+    path('generate_captcha/', views.generate_captcha, name='generate_captcha'),
 ]
-
